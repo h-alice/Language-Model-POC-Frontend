48cc95bcbf8bef85e7ba171baf1b5699f280e8ab
h-alice <nt10698666@gapp.nthu.edu.tw>
2024-04-11 07:16:14 +0000

    Now we can start backent in `screen` session.

--- backend_service.sh
+++ backend_service.sh
@@ -1,16 +1,2 @@
-docker run --gpus all \
-           -p 15820:80 \
-           -v /home/q36862/model_repos:/models ghcr.io/huggingface/text-embeddings-inference:turing-0.6 \
-           --model-id /models/multilingual-e5-large
-
-docker run --gpus all \
-           --shm-size 1g \
-           -p 15810:80 \
-           -v /home/q36862/data:/data \
-           -v /home/q36862/model_repos:/models ghcr.io/huggingface/text-generation-inference:1.3 \
-           --model-id /models/M7 \
-           --max-concurrent-requests 1 \
-           --max-total-tokens 4096 \
-           --max-input-length 2048 \
-           --num-shard 1 \
-           --quantize bitsandbytes-nf4
+screen -S emb-serv -d -m bash -c 'sudo bash ./embedding-service.sh'
+screen -S tgi-serv -d -m bash -c 'sudo bash ./text-generation-service.sh'
