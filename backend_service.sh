969e840f62cbd2c155954fb187820d8f60b01f09
h-alice <nt10698666@gapp.nthu.edu.tw>
2024-04-11 06:59:51 +0000

    Startup script.

---
+++ backend_service.sh
@@ -0,0 +1,2 @@
+docker run --gpus all -p 15820:80 -v /home/q36862/model_repos:/models ghcr.io/huggingface/text-embeddings-inference:turing-0.6 --model-id /models/multilingual-e5-large
+docker run --gpus all --shm-size 1g -p 15810:80 -v /home/q36862/data:/data -v /home/q36862/model_repos:/models ghcr.io/huggingface/text-generation-inference:1.3 --model-id /models/M7 --max-concurrent-requests 1 --max-total-tokens 4096 --max-input-length 2048 --num-shard 1 --quantize bitsandbytes-nf4
